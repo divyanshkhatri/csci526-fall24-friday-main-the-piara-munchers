bdc431cdc4254a0b0fa79c601d23f6a9bb6fcf8d
Shaurya Singh <156408028+Amisho11@users.noreply.github.com>
2024-11-13 23:30:00 -0800

    tutorial and level 3 blinking changes

--- Assets/Scripts/ShowFlipMessage.cs
+++ Assets/Scripts/ShowFlipMessage.cs
@@ -9,6 +9,7 @@ public class ShowFlipMessage : MonoBehaviour
     public TextMeshProUGUI messageText;
     public Image backgroundImage;
     public bool collisionOccured = false;
+
     void Start()
     {
         if (GetComponent<Collider2D>() == null)
@@ -21,11 +22,10 @@ public class ShowFlipMessage : MonoBehaviour
             Rigidbody2D rb = gameObject.AddComponent<Rigidbody2D>();
             rb.isKinematic = true;
         }
-    }
 
-    void Update()
-    {
-
+        // Initially, hide the message text and background image
+        messageText.enabled = false;
+        backgroundImage.enabled = false;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -33,17 +33,22 @@ public class ShowFlipMessage : MonoBehaviour
         if (!collisionOccured && collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Collision detected");
-            StartCoroutine(ShowMessage());
+            ShowMessage();
             collisionOccured = true;
         }
     }
 
-    IEnumerator ShowMessage()
+    void ShowMessage()
     {
-        messageText.text = "Guess, you should flip the world!";
+        messageText.text = "Hitting the lazer kills you instantly";
         backgroundImage.enabled = true;
         messageText.enabled = true;
-        yield return new WaitForSeconds(2);
+        StartCoroutine(HideMessageAfterDelay());
+    }
+
+    IEnumerator HideMessageAfterDelay()
+    {
+        yield return new WaitForSeconds(3);
         messageText.enabled = false;
         backgroundImage.enabled = false;
     }
